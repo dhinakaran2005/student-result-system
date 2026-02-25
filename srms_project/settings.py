1d040f60444c269f491e8eb462ac05fed72b1c9d
Dhinakaran <yourgithubemail@gmail.com>
2026-02-25 13:57:27 +0530

    Fixed static configuration

--- srms_project/settings.py
+++ srms_project/settings.py
@@ -104,7 +104,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ADMIN_SITE_HEADER = "Student Result Management System"
 ADMIN_SITE_TITLE = "SRMS Admin Dashboard"
