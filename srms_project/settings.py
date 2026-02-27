9ff6082182268a7728b271c08f704328a3426f66
Dhinakaran <yourgithubemail@gmail.com>
2026-02-27 07:28:12 +0530

    Added whitenoise

--- srms_project/settings.py
+++ srms_project/settings.py
@@ -31,12 +31,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ...
 ]
 
 ROOT_URLCONF = 'srms_project.urls'
@@ -106,6 +104,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ADMIN_SITE_HEADER = "Student Result Management System"
 ADMIN_SITE_TITLE = "SRMS Admin Dashboard"
